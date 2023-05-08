6c6edc65c47589b9de266614e2ea5fcf14b19496
TylerMorgan142 <tylermorgan142@gmail.com>
2023-05-08 19:07:45 +0000

    Add funtion to print game board

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+player_board = [["."] * 10 for x in range(10)]
+computer_board = [["."] * 10 for x in range(10)]
+
+def print_board(board):
+    print("0 1 2 3 4 5 6 7 8 9")
+    print("*******************")
+    for row in board:
+        print(" ".join(row))
+
+
+
+print_board(player_board)        
+print_board(computer_board)
