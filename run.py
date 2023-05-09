6b59475add472a09ff9470e5ffbc51a1b6486bf6
TylerMorgan142 <tylermorgan142@gmail.com>
2023-05-09 21:56:10 +0000

    Add function to populate board with ships

--- run.py
+++ run.py
@@ -4,12 +4,33 @@ player_board = [["."] * 10 for x in range(10)]
 computer_board = [["."] * 10 for x in range(10)]
 
 def print_board(board):
+    """
+    Prints the board where the game will take place.
+    """
     print("0 1 2 3 4 5 6 7 8 9")
-    print("*******************")
     for row in board:
         print(" ".join(row))
 
 
 
-print_board(player_board)        
+def add_ships(board):
+    """
+    Populates the board with ships in random locations.
+    """
+    for ship in range(10):
+        row = randint(0, 9)
+        column = randint(0, 9)
+        while board[row][column] == "*":
+            row = randint(0, 9)
+            column = randint(0, 9)   
+        board[row][column] = "*"
+
+
+
+
+add_ships(player_board)
+print_board(player_board)
 print_board(computer_board)
+
+
+
