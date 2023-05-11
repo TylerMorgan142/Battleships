ca8a17ad74a13383280a75aca8baf098351de621
TylerMorgan142 <tylermorgan142@gmail.com>
2023-05-11 00:59:19 +0000

    Add function for the players guess

--- run.py
+++ run.py
@@ -25,12 +25,34 @@ def add_ships(board):
             column = randint(0, 9)   
         board[row][column] = "*"
 
-
-
+def player_guess():
+    """
+    Gets the players input for their guess of a row and column.
+    """
+    try:
+        guess_row = int(input("Please enter a row between 0-9\n"))
+        while  guess_row not in range(0, 9):
+            print("Row must be between 0-9")
+            guess_row = int(input("Please enter a row between 0-9\n"))
+            return guess_row    
+    except ValueError:
+        print("Input must be a number between 0-9")
+        player_guess()
+    
+    try:
+        
+        guess_column = int(input("Please enter a column between 0-9\n"))
+        while  guess_column not in range(0, 9):
+            print("Column must be between 0-9")
+            guess_row = int(input("Please enter a column between 0-9\n"))
+            return guess_column    
+    except ValueError:
+        print("Input must be a number between 0-9")
+        player_guess()
 
 add_ships(player_board)
 print_board(player_board)
 print_board(computer_board)
-
+player_guess()
 
 
