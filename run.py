6d11201af734a9a02cbdb6e6a05b2216135909e2
TylerMorgan142 <tylermorgan142@gmail.com>
2023-05-12 05:55:53 +0000

    Add functions to add computer ships and check the player's guess against the computer's board.

--- run.py
+++ run.py
@@ -1,58 +1,89 @@
 from random import randint
 
-player_board = [["."] * 10 for x in range(10)]
-computer_board = [["."] * 10 for x in range(10)]
+player_board = [["."] * 5 for x in range(5)]
+computer_board = [["."] * 5 for x in range(5)]
 
+player_score = 0
+computer_score = 0
+
+print(f"Player score = {player_score}")
+print(f"Computer_score = {computer_score}")
 def print_board(board):
     """
     Prints the board where the game will take place.
     """
-    print("0 1 2 3 4 5 6 7 8 9")
+    print("0 1 2 3 4")
     for row in board:
         print(" ".join(row))
 
 
 
-def add_ships(board):
+def add_player_ships(board):
     """
     Populates the board with ships in random locations.
     """
-    for ship in range(10):
-        row = randint(0, 9)
-        column = randint(0, 9)
+    for ship in range(5):
+        row = randint(0, 4)
+        column = randint(0, 4)
         while board[row][column] == "*":
-            row = randint(0, 9)
-            column = randint(0, 9)   
+            row = randint(0, 4)
+            column = randint(0, 4)   
         board[row][column] = "*"
+
+def add_computer_ships():
+
+
+    for ship in range(5):
+        row = randint(0, 4)
+        column = randint(0, 4)
+        return row, column
+
+
+
 
 def player_guess():
     """
     Gets the players input for their guess of a row and column.
     """
     try:
-        guess_row = int(input("Please enter a row between 0-9\n"))
-        while  guess_row not in range(0, 9):
-            print("Row must be between 0-9")
-            guess_row = int(input("Please enter a row between 0-9\n"))
-            return guess_row    
+        guess_row = int(input("Please enter a row between 0-4\n"))
+        while  guess_row not in range(0, 4):
+            print("Row must be between 0-4")
+            guess_row = int(input("Please enter a row between 0-4\n"))    
     except ValueError:
-        print("Input must be a number between 0-9")
+        print("Input must be a number between 0-4")
         player_guess()
     
     try:
         
-        guess_column = int(input("Please enter a column between 0-9\n"))
-        while  guess_column not in range(0, 9):
-            print("Column must be between 0-9")
-            guess_row = int(input("Please enter a column between 0-9\n"))
+        guess_column = int(input("Please enter a column between 0-4\n"))
+        while  guess_column not in range(0, 4):
+            print("Column must be between 0-4")
+            guess_row = int(input("Please enter a column between 0-4\n"))
             return guess_column    
     except ValueError:
-        print("Input must be a number between 0-9")
+        print("Input must be a number between 0-4")
         player_guess()
+    return guess_row, guess_column
 
-add_ships(player_board)
+def check_for_hits():
+    computer_ships = add_computer_ships()
+    guess = player_guess()
+    if guess == computer_ships:
+        print("Congratlations you hit a ship!")
+        global player_score
+        player_score += 1
+    else:
+        print("You missed!")     
+
+
+
+
+
+add_player_ships(player_board)
 print_board(player_board)
 print_board(computer_board)
-player_guess()
+check_for_hits()
+
 
 
